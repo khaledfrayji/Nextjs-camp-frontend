2b808690977bcabed2acfccb5b7e9a695face50f
khaled frayji <121765804+khaledfrayji@users.noreply.github.com>
2024-08-24 14:47:12 +0300

    add key prop for an element in an iterator in the Footer.tsx file.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,11 +13,11 @@ const Footer = () => {
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
+            {FOOTER_LINKS.map((columns, index) => (
+              <FooterColumn key={index} title={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
+                  {columns.links.map((link, linkIndex) => (
+                    <Link href="/" key={linkIndex}>
                       {link}
                     </Link>
                   ))}
@@ -27,10 +27,10 @@ const Footer = () => {
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-                {FOOTER_CONTACT_INFO.links.map((link) => (
+                {FOOTER_CONTACT_INFO.links.map((link, linkIndex) => (
                   <Link
                     href="/"
-                    key={link.label}
+                    key={linkIndex}
                     className="flex gap-4 md:flex-col lg:flex-row"
                   >
                     <p className="whitespace-nowrap">
@@ -47,8 +47,8 @@ const Footer = () => {
             <div className="flex flex-col gap-5">
               <FooterColumn title={SOCIALS.title}>
                 <ul className="regular-14 flex gap-4 text-gray-30">
-                  {SOCIALS.links.map((link) => (
-                    <Link href="/" key={link}>
+                  {SOCIALS.links.map((link, linkIndex) => (
+                    <Link href="/" key={linkIndex}>
                       <Image src={link} alt="logo" width={24} height={24} />
                     </Link>
                   ))}
